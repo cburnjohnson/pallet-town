a5a5bbc0526cfd2a70957ed1dbd24e03b7f378af
cburnjohnson <cburnjohnson@gmail.com>
2023-01-04 13:36:05 -0800

    basic movement

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -8,43 +8,163 @@
 </template>
 
 <script>
-import mapImage from '@/assets/imgs/game-portfolio-map.png';
-import playerSpriteImage from '@/assets/sprites/player/playerDown.png';
+import mapImageAsset from '@/assets/imgs/game-portfolio-map.png';
+import playerImageAsset from '@/assets/sprites/player/playerDown.png';
+
+class Sprite {
+  constructor({ position, image }) {
+    this.position = position;
+    this.image = image;
+  }
+
+  draw() {
+    context.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
+
+let context;
+let lastKeyPressed;
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+};
 
 export default {
   data() {
     return {
       gameCanvas: null,
-      context: null
+      mapImage: null,
+      playerImage: null,
+      playerSprite: null
     };
   },
   mounted() {
     this.gameCanvas = document.querySelector('#gameCanvas');
-    this.context = this.gameCanvas.getContext('2d');
+    context = this.gameCanvas.getContext('2d');
 
-    this.gameCanvas.width = 980;
+    this.gameCanvas.width = 840;
     this.gameCanvas.height = 800;
 
-    const image = new Image();
-    image.src = mapImage;
+    this.mapImage = new Image();
+    this.mapImage.src = mapImageAsset;
 
-    const playerImage = new Image();
-    playerImage.src = playerSpriteImage;
+    this.playerImage = new Image();
+    this.playerImage.src = playerImageAsset;
 
-    image.onload = () => {
-      this.context.drawImage(image, 0, -100);
-      this.context.drawImage(
-        playerImage,
+    this.mapSprite = new Sprite({
+      position: {
+        x: 0,
+        y: -100
+      },
+      image: this.mapImage
+    });
+
+    this.animate();
+
+    this.addEventListeners();
+  },
+  methods: {
+    addEventListeners() {
+      window.addEventListener('keydown', (event) => {
+        switch (event.key) {
+          case 'w':
+            keys.w.pressed = true;
+            lastKeyPressed = 'w';
+
+            break;
+
+          case 'a':
+            keys.a.pressed = true;
+            lastKeyPressed = 'a';
+
+            break;
+
+          case 's':
+            keys.s.pressed = true;
+            lastKeyPressed = 's';
+
+            break;
+
+          case 'd':
+            keys.d.pressed = true;
+            lastKeyPressed = 'd';
+
+            break;
+
+          default:
+            break;
+        }
+      });
+
+      window.addEventListener('keyup', (event) => {
+        switch (event.key) {
+          case 'w':
+            keys.w.pressed = false;
+
+            break;
+
+          case 'a':
+            keys.a.pressed = false;
+
+            break;
+
+          case 's':
+            keys.s.pressed = false;
+
+            break;
+
+          case 'd':
+            keys.d.pressed = false;
+
+            break;
+
+          default:
+            break;
+        }
+      });
+    },
+    animate() {
+      window.requestAnimationFrame(this.animate);
+      this.mapSprite.draw();
+
+      context.drawImage(
+        this.playerImage,
         0,
         0,
-        playerImage.width / 4,
-        playerImage.height,
-        this.gameCanvas.width / 2 - playerImage.width / 2,
+        this.playerImage.width / 4,
+        this.playerImage.height,
+        this.gameCanvas.width / 2 - this.playerImage.width / 4 / 2,
         this.gameCanvas.height / 2,
-        playerImage.width / 4,
-        playerImage.height
+        this.playerImage.width / 4,
+        this.playerImage.height
       );
-    };
+
+      if (keys.w.pressed && lastKeyPressed === 'w') {
+        this.mapSprite.position.y += 3;
+      }
+
+      if (keys.a.pressed && lastKeyPressed === 'a') {
+        this.mapSprite.position.x += 3;
+      }
+
+      if (keys.s.pressed && lastKeyPressed === 's') {
+        this.mapSprite.position.y -= 3;
+      }
+
+      if (keys.d.pressed && lastKeyPressed === 'd') {
+        this.mapSprite.position.x -= 3;
+      }
+    }
   }
 };
 </script>
