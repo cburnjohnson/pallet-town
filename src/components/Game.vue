e4f100fde83f4f7cedc8c8f183501aed6e12dec5
cburnjohnson <cburnjohnson@gmail.com>
2023-01-03 22:17:03 -0800

    setting up map

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,18 +1,39 @@
 <template>
-  <canvas id="gameCanvas" />
+  <div class="game">
+    <canvas id="gameCanvas" />
+  </div>
 </template>
 
 <script>
+import mapImage from '@/assets/imgs/game-portfolio-map.png';
+
 export default {
   data() {
     return {
-      gameCanvas: null
+      gameCanvas: null,
+      context: null
     };
   },
   mounted() {
     this.gameCanvas = document.querySelector('#gameCanvas');
+    this.context = this.gameCanvas.getContext('2d');
+
+    this.gameCanvas.width = 1024;
+    this.gameCanvas.height = 576;
+
+    const image = new Image();
+    image.src = mapImage;
+
+    image.onload = () => {
+      this.context.drawImage(image, -100, -250);
+    };
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.game {
+  display: flex;
+  justify-content: center;
+}
+</style>
