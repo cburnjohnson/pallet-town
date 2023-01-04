9ad060e25be917fafe75866b9d30f9aecd71f0a5
cburnjohnson <cburnjohnson@gmail.com>
2023-01-04 13:12:11 -0800

    rendering player sprite

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -22,22 +22,29 @@ export default {
     this.gameCanvas = document.querySelector('#gameCanvas');
     this.context = this.gameCanvas.getContext('2d');
 
-    this.gameCanvas.width = 1024;
+    this.gameCanvas.width = 980;
     this.gameCanvas.height = 800;
 
     const image = new Image();
     image.src = mapImage;
 
-    image.onload = () => {
-      this.context.drawImage(image, -100, -250);
-
-      playerImage.onload = () => {
-        this.context.drawImage(playerImage, 150, 250);
-      };
-    };
-
     const playerImage = new Image();
     playerImage.src = playerSpriteImage;
+
+    image.onload = () => {
+      this.context.drawImage(image, 0, -100);
+      this.context.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        this.gameCanvas.width / 2 - playerImage.width / 2,
+        this.gameCanvas.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+      );
+    };
   }
 };
 </script>
