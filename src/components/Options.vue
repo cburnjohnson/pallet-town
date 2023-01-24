607621873996ab1ff63d76c4ef30c8e42549e1f2
cburnjohnson <cburnjohnson@gmail.com>
2023-01-23 21:57:17 -0800

    styling for options list

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -3,8 +3,11 @@
     <ul class="options__list">
       <li
         v-for="option in npcOptions"
-        :key="option.id"
-        class="options__item"
+        :key="option.name"
+        :class="[
+          'options__item',
+          { 'options__item--active': isOptionActive(option.name) }
+        ]"
         @click="setActiveOption(option)"
       >
         <button class="option__button">
@@ -41,6 +44,9 @@ export default {
   methods: {
     setActiveOption(option) {
       this.activeNPC.interactions.option.active = option;
+    },
+    isOptionActive(optionName) {
+      return this.activeOption.name === optionName;
     }
   }
 };
@@ -59,7 +65,30 @@ export default {
     list-style: none;
     background-color: white;
     padding: 24px;
-    border-radius: $border-radius;
+    border-radius: 4px;
+    border: 4px solid black;
+    min-width: 270px;
+  }
+
+  &__item {
+    position: relative;
+    margin-top: 12px;
+
+    &:first-child {
+      margin-top: 0px;
+    }
+
+    &--active {
+      &::before {
+        content: '';
+        position: absolute;
+        border-style: solid;
+        border-width: 8px 12px 8px 12px;
+        border-color: transparent transparent transparent black;
+        position: absolute;
+        left: -16px;
+      }
+    }
   }
 }
 </style>
