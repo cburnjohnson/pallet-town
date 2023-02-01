d0289c0fabbbad877870b6a91a3fdf1781bc14ff
cburnjohnson <cburnjohnson@gmail.com>
2023-01-31 21:39:52 -0800

    added projects

--- src/library/projects.js
+++ src/library/projects.js
@@ -1,5 +1,6 @@
 export default [
   {
+    id: 1,
     name: 'K. Holmberg Art',
     description:
       'K. Holmberg\'s art demo eCommerce site built with the MERN stack.',
@@ -18,20 +19,188 @@ export default [
     category: 'mern'
   },
   {
-    name: 'Where To Eat',
+    id: 2,
+    name: 'Expense Tracker',
     description:
-      'Restaurant application that displays a random nearby restaurant and information about the restaurant.',
-    technologyUsed: ['React', 'NodeJS', 'MongoDB', 'Express', 'CSS', 'Stripe'],
+      'MERN Stack application that keeps track of the user\'s expenses.',
+    technologyUsed: ['React', 'NodeJS', 'MongoDB', 'Express', 'CSS', 'JWT'],
+    thumbnail: '../src/assets/projects/kHolmbergArt.jpeg',
     links: [
       {
         name: 'Live',
-        url: 'https://kholmberg.herokuapp.com/'
+        url: 'https://expensetracker47.herokuapp.com/'
       },
       {
         name: 'Github',
-        url: 'https://github.com/cburnjohnson/k-holmberg'
+        url: 'https://github.com/cburnjohnson/expense-tracker'
+      }
+    ],
+    category: 'mern'
+  },
+  {
+    id: 3,
+    name: 'Watch N Track',
+    description:
+      'MERN Stack application that tracks the user\'s movies, TV shows, and anime they have watched.',
+    technologyUsed: ['React', 'NodeJS', 'MongoDB', 'Express', 'CSS', 'JWT'],
+    thumbnail: '../src/assets/projects/kHolmbergArt.jpeg',
+    links: [
+      {
+        name: 'Live',
+        url: 'https://watchntrack.herokuapp.com/'
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/cburnjohnson/watch-n-track'
+      }
+    ],
+    category: 'mern'
+  },
+  {
+    id: 4,
+    name: 'Phone Book',
+    description:
+      'MERN Stack phone book application that stores user\'s personal professional contacts.',
+    technologyUsed: ['React', 'NodeJS', 'MongoDB', 'Express', 'CSS', 'JWT'],
+    thumbnail: '../src/assets/projects/kHolmbergArt.jpeg',
+    links: [
+      {
+        name: 'Live',
+        url: 'https://phonebook47.herokuapp.com/'
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/cburnjohnson/phone-book'
+      }
+    ],
+    category: 'mern'
+  },
+  {
+    name: 'Where To Eat',
+    description:
+      'Restaurant application that displays a random nearby restaurant and information about the restaurant.',
+    technologyUsed: ['React', 'JavaScript', 'CSS', 'Bootstrap', 'Google Places API'],
+    links: [
+      {
+        name: 'Live',
+        url: 'https://wheretoeat.netlify.app/'
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/cburnjohnson/where-to-eat'
       }
     ],
     category: 'React'
+  },
+  {
+    name: 'Pokedex',
+    description:
+      'An application that emulates a Pokédex.',
+    technologyUsed: ['React', 'JavaScript', 'CSS'],
+    links: [
+      {
+        name: 'Live',
+        url: 'https://pokedex123.netlify.app/'
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/cburnjohnson/pokedex'
+      }
+    ],
+    category: 'React'
+  },
+  {
+    name: 'Text Racer',
+    description:
+      'Text Racer is a game that challenges how fast you can type on your mobile device.',
+    technologyUsed: ['React Native', 'JavaScript', 'Useless Facts API'],
+    links: [
+      {
+        name: 'Github',
+        url: 'https://github.com/cburnjohnson/text-racer'
+      }
+    ],
+    category: 'React Native'
+  },
+  {
+    name: 'Meme Dispense',
+    description:
+      'Dispenses random memes.',
+    technologyUsed: ['JavaScript', 'HTML', 'CSS'],
+    links: [
+      {
+        name: 'Live',
+        url: 'https://cburnjohnson.github.io/meme-dispenser/'
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/cburnjohnson/meme-dispenser'
+      }
+    ],
+    category: 'JavaScript'
+  },
+  {
+    name: 'Quote of the Day',
+    description:
+      'Generates the quote of the day.',
+    technologyUsed: ['JavaScript', 'HTML', 'CSS'],
+    links: [
+      {
+        name: 'Live',
+        url: 'https://cburnjohnson.github.io/quote-of-the-day/'
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/cburnjohnson/quote-of-the-day'
+      }
+    ],
+    category: 'JavaScript'
+  },
+  {
+    name: 'BMI Calculator',
+    description:
+      'Calculates your BMI.',
+    technologyUsed: ['JavaScript', 'HTML', 'CSS'],
+    links: [
+      {
+        name: 'Live',
+        url: 'https://cburnjohnson.github.io/bmi-calculator/'
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/cburnjohnson/bmi-calculator'
+      }
+    ],
+    category: 'JavaScript'
+  },
+  {
+    name: 'To Do List',
+    description:
+      'To Do List application that records tasks and moves them over to the done section when completed.',
+    technologyUsed: ['JavaScript', 'HTML', 'CSS'],
+    links: [
+      {
+        name: 'Live',
+        url: 'https://cburnjohnson.github.io/to-do-list/'
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/cburnjohnson/to-do-list'
+      }
+    ],
+    category: 'JavaScript'
+  },
+  {
+    name: 'Cloud the Pom',
+    description:
+      'A website created for my dog that allows me to login and upload pictures that get stored in a picture library directory. The homepage has a carousel populated with random pictures from the picture library.',
+    technologyUsed: ['PHP', 'JavaScript', 'HTML', 'CSS'],
+    links: [
+      {
+        name: 'Github',
+        url: 'https://github.com/cburnjohnson/cloud-the-pom/'
+      }
+    ],
+    category: 'JavaScript'
   }
 ];
